bd84cad6cc55213c68185bec43d72cd8bd3029e4
Masahiko Kaiho <mkaiho.cgengo@gmail.com>
2019-12-22 17:29:02 +0900

    generate all codes with Spring Initializr.

---
+++ src/main/java/com/mkaiho/learning/videoencoder/VideoEncoderJavaApplication.java
@@ -0,0 +1,13 @@
+package com.mkaiho.learning.videoencoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoEncoderJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoEncoderJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mkaiho/learning/videoencoder/VideoEncoderJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkaiho.learning.videoencoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoEncoderJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
