7e90b672afcd2d2104f4b2744776ea4089581356
Masahiko Kaiho <mkaiho.cgengo@gmail.com>
2019-12-23 00:54:09 +0900

    organize directory structure to multi-module project.

---
+++ batch/src/main/java/com/mkaiho/learning/videoencoder/batch/BatchApplication.java
@@ -0,0 +1,13 @@
+package com.mkaiho.learning.videoencoder.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchApplication.class, args);
+	}
+
+}
